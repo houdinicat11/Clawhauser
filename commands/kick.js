67076ba938029f15f75951b69d0346921196f409
Houdinicat11 <webstudent31@gmail.com>
2019-06-05 12:55:00 -0500

    make it better

--- commands/kick.js
+++ commands/kick.js
@@ -1,7 +1,18 @@
-const kick = require('../commands/kick')
+module.exports = message => {
+  const member = message.mentions.members.first();
 
-module.exports = (client, message) => {
-  if (message.content.startsWith('!kick')) {
-    return kick(message)
+  if (!member) {
+    return message.reply(
+      `Who are you trying to kick? You must mention a user.`
+    );
   }
-}
+
+  if (!member.kickable) {
+    return message.reply(`I can't kick this user. Sorry!`);
+  }
+
+  return member
+    .kick()
+    .then(() => message.reply(`${member.user.tag} was kicked.`))
+    .catch(error => message.reply(`Sorry, an error occured.`));
+};
--- index.js
+++ index.js
@@ -1,46 +1,15 @@
-require('dotenv').config()
-const fs = require('fs')
-const Discord = require('discord.js')
-const client = new Discord.Client()
+require("dotenv").config();
+const Discord = require("discord.js");
+const fs = require("fs");
+const client = new Discord.Client();
 
-fs.readdir('./events/', (err, files) => {
+fs.readdir("./events/", (err, files) => {
   files.forEach(file => {
-    const eventHandler = require(`./events/${file}`)
-    const eventName = file.split('.')[0]
-    client.on(eventName, (...args) => eventHandler(client, ...args))
-  })
-})
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`)
-})
-
-client.on('guildMemberAdd', member => {
-  member.send(
-    `Welcome on the server! Please be aware that we won't tolerate troll, spam or harassment. Have fun 😀`
-  )
-})
-
-client.on('message', message => {
-  if (message.content.startsWith('!kick')) {
-    const member = message.mentions.members.first()
-
-    if (!member) {
-      return message.reply(
-        `Who are you trying to kick? You must mention a user.`
-      )
-    }
-
-    if (!member.kickable) {
-      return message.reply(`I can't kick this user. Sorry!`)
-    }
-
-    return member
-      .kick()
-      .then(() => message.reply(`${member.user.tag} was kicked.`))
-      .catch(error => message.reply(`Sorry, an error occured.`))
-  }
-})
+    const eventHandler = require(`./events/${file}`);
+    const eventName = file.split(".")[0];
+    client.on(eventName, (...args) => eventHandler(client, ...args));
+  });
+});
 
 client.on('message', msg => {
   if (msg.content === 'ping') {
@@ -48,4 +17,5 @@ client.on('message', msg => {
   }
 })
 
-client.login(process.env.BOT_TOKEN)
+
+client.login(process.env.BOT_TOKEN);
