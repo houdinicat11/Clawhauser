1d218a4791dd9bd89e5ab19ce00c209a291f07dc
Houdinicat11 <webstudent31@gmail.com>
2019-06-05 13:39:23 -0500

    make it better

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,5 @@
 module.exports = (client, member) => {
   member.send(
-    `Welcome to the Server! All things go here so kick back and have fun, meow!
+    `Welcome to the Server! All things go here so kick back and have fun, meow!`
   );
 };
