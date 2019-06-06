e7f51cbf39fc4086a19f1bedeac89f9403453767
Houdinicat11 <webstudent31@gmail.com>
2019-06-06 13:46:31 -0500

    adding ban functionality

--- commands/ban.js
+++ commands/ban.js
@@ -1,21 +1,6 @@
 module.exports = message => {
 	const member = message.mentions.members.first();
-
-	if (member) {
-        member.ban({
-          reason: 'They were bad!',
-        }).then(() => {
-          // We let the message author know we were able to ban the person
-          message.reply(`Successfully banned ${user.tag}`);
-        }).catch(err => {
-          // An error happened
-          // This is generally due to the bot not being able to ban the member,
-          // either due to missing permissions or role hierarchy
-          message.reply('I was unable to ban the member');
-          // Log the error
-          console.error(err);
-        });
-      }
+	const reason = "No reason provided";
 	
 	if (!member) 
 	{
@@ -27,7 +12,7 @@ module.exports = message => {
 	}
 		
 	return member
-		.ban()
+		.ban(reason)
 		.then(() => message.reply(`${member.user.tag} was banned.`))
 		.catch(error => message.reply('Sorry, an error occured.'));
 };
