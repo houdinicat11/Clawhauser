c9b45ac4703701c717419222ae7970e7b471818f
Houdinicat11 <webstudent31@gmail.com>
2019-06-06 12:28:31 -0500

    starting to add back music bot functionality

--- commands/kick.js
+++ commands/kick.js
@@ -1,4 +1,23 @@
-module.exports = 
+module.exports = message => {
+  const member = message.mentions.members.first();
+
+  if (!member) {
+    return message.reply(
+      `Who are you trying to kick? You must mention a user.`
+    );
+  }
+
+  if (!member.kickable) {
+    return message.reply(`I can't kick this user. Sorry!`);
+  }
+
+  return member
+    .kick()
+    .then(() => message.reply(`\n LEAVE THIS PLACE!!! \n ${member.user.tag} was kicked.`))
+    .catch(error => message.reply(`Sorry, an error occured.`));
+};
+
+/*module.exports = 
 {
 	name: 'kick',
 	description: 'Kick a player',
@@ -23,4 +42,4 @@ module.exports =
 			.then(() => message.reply(`\n LEAVE THIS PLACE!!! \n ${member.user.tag} was kicked.`))
 			.catch(error => message.reply(`Sorry, an error occured.`));
 	},
-};
+};*/
