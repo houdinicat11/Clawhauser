482d280cd0e6010a6823b526c91a950937fa9bd1
Houdinicat11 <webstudent31@gmail.com>
2019-06-06 12:40:18 -0500

    adding ban functionality

--- commands/ban.js
+++ commands/ban.js
@@ -1,20 +1,17 @@
-module.exports = {
-	name: 'ban',
-	description: 'Ban a player',
-	execute(message) {
-		const member = message.mentions.members.first();
+module.exports = message => {
+	const member = message.mentions.members.first();
 
-		if (!member) {
-			return message.reply('You need to mention the member you want to ban him');
-		}
+	if (!member) 
+	{
+		return message.reply('You need to mention the member you want to ban him');
+	}
 
-		if (!member.banable) {
-			return message.reply('I can\'t ban this user.');
-		}
-
-		return member
-			.ban()
-			.then(() => message.reply(`${member.user.tag} was banned.`))
-			.catch(error => message.reply('Sorry, an error occured.'));
-	},
+	if (!member.banable) {
+		return message.reply('I can\'t ban this user.');
+	}
+		
+	return member
+		.ban()
+		.then(() => message.reply(`${member.user.tag} was banned.`))
+		.catch(error => message.reply('Sorry, an error occured.'));
 };
--- events/message.js
+++ events/message.js
@@ -1,7 +1,14 @@
 const kick = require("../commands/kick");
+const ban = require("../commands/ban");
 
 module.exports = (client, message) => {
   if (message.content.startsWith("!kick")) {
     return kick(message);
   }
+};
+
+module.exports = (client, message) => {
+	if(message.content.startsWith("!ban")) {
+		return kick(message);
+	}
 };
