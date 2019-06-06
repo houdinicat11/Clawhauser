5f6d314d8f28088a13295cdb1bf714216445c74a
Houdinicat11 <webstudent31@gmail.com>
2019-06-06 14:44:28 -0500

    adding ban functionality

--- commands/ban.js
+++ commands/ban.js
@@ -1,7 +1,7 @@
 module.exports = message => {
 	const member = message.mentions.members.first();
 
-	if (member) {
+	/*if (member) {
         member.ban({
           reason: 'They were bad!',
         }).then(() => {
@@ -14,19 +14,19 @@ module.exports = message => {
           // Log the error
           console.error(err);
         });
-      }
+      }*/
 	
 	if (!member) 
 	{
 		return message.reply('You need to mention the member you want to ban him');
 	}
 
-	if (!member.banable) {
-		//return message.reply('I can\'t ban this user.');
+	if (!member.bannable) {
+		return message.reply('I can\'t ban this user.');
 	}
 		
 	return member
 		.ban()
 		.then(() => message.reply(`${member.user.tag} was banned. Good Riddence`))
-		.catch(error => message.reply('\n'));
+		.catch(error => message.reply('I\'ve encountered an error'));
 };
