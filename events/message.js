f2f5ddcebdd05be791f068282f3ae9a49d37dfca
Houdinicat11 <webstudent31@gmail.com>
2019-06-17 15:20:18 -0500

    trying to get kick and ban back up

--- events/message.js
+++ events/message.js
@@ -2,7 +2,7 @@ const kick = require("../commands/kick");
 const ban = require("../commands/ban");
 
 module.exports = (client, message) => {
-  if (message.content.startsWith("$`kick")) {
+  if (message.content.startsWith("`kick")) {
     return kick(message);
   }
 };
